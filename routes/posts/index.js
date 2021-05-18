7ba0ff3d703b192c94e90eb9e009514279c2e0d8
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2021-05-18 18:26:01 +1200

    ok

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -1,5 +1,8 @@
+const Post = require('../../models/Posts');
+
 module.exports = (route) => {
   route.get('/posts', (request, response) => {
+    Post.create({ title: 'H3ll0 W04ld' });
     response.send('Post Index');
   });
 };
